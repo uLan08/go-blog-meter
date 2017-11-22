758e04d520bd299337851d8b9585c199eebbf6ce
Patrick Rainier Juen <patrickrainier@gmail.com>
2017-11-22 16:44:11 +0800

    Strip and clean out html

--- blogmeter.go
+++ blogmeter.go
@@ -1,24 +1,44 @@
-package blogmeter
+package main
 
 import (
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
-	"mvdan.cc/xurls"
+	"github.com/grokify/html-strip-tags-go"
 )
 
-func main() {
-	resp, err := http.Get("http://lauftechnik.de/")
+func cleanStr(str *string) {
+	re := regexp.MustCompile(`\r?\n`)
+	*str = strip.StripTags(*str)
+	*str = re.ReplaceAllString(strings.Replace(*str, "&nbsp;", "", -1), "")
+	*str = strings.Replace(*str, "\t", "", -1)
+}
+
+func getBody(url string) string {
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	str := string(body)
-	urls := xurls.Strict().FindAllString(str, -1)
-	for _, url := range urls {
-		fmt.Println(url)
+	return string(body)
+}
+
+func main() {
+	str := getBody("http://lauftechnik.de/")
+	cleanStr(&str)
+	words := strings.Split(str, " ")
+	for _, word := range words {
+		fmt.Println(word)
 	}
+
+	// fmt.Println(words, len(words))
+	// urls := xurls.Strict().FindAllString(str, -1)
+	// for _, url := range urls {
+	// 	fmt.Println(url)
+	// }
 }
